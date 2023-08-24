a52a9904d1d72070b1e6cd16103ab49bd2025237
vipinsunny <vipinsunny96@gmail.com>
2023-08-24 16:28:45 +0530

    save new

--- backend_sanity/sanity.config.ts
+++ backend_sanity/sanity.config.ts
@@ -2,8 +2,6 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas/index'
-import {netlifyWidget} from 'sanity-plugin-dashboard-widget-netlify'
-import {dashboardTool} from '@sanity/dashboard'
 
 export default defineConfig({
   name: 'default',
@@ -12,34 +10,9 @@ export default defineConfig({
   projectId: '42e9awfe',
   dataset: 'production',
 
+  plugins: [deskTool(), visionTool()],
+
   schema: {
     types: schemaTypes,
   },
-  plugins: [
-    deskTool(),
-    visionTool(),
-    dashboardTool({
-      widgets: [
-        netlifyWidget({
-          title: 'My Netlify deploys',
-          sites: [
-            {
-              title: 'Sanity Studio',
-              apiId: '711ce701-d9c6-4b59-a463-4472a087839a',
-              buildHookId: 'https://api.netlify.com/build_hooks/64e72f4386de4d644e3742da',
-              name: 'silly-piroshki-ba97d3',
-            },
-            {
-              title: 'Sanity Studio',
-              apiId: '711ce701-d9c6-4b59-a463-4472a087839a',
-              buildHookId: 'https://api.netlify.com/build_hooks/64e72f4386de4d644e3742da',
-              name: 'silly-piroshki-ba97d3',
-              url: 'https://silly-piroshki-ba97d3.netlify.app',
-            },
-          ],
-        }),
-      ],
-    }),
-  ],
 })
-
